6c6cdb0f450c483cd555374bba95a085aaa3f83e
pablo2240 <pablo2240@DESKTOP-A4A7IQB>
2025-05-16 11:51:17 -0500

    el nuevo

---
+++ Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Shared.Entites;
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
