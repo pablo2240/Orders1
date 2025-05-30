9d837382b34e461dbec9d95abe6c654bd7cbd656
pablo2240 <pablo2240@DESKTOP-A4A7IQB>
2025-05-30 14:33:36 -0500

    dia 30 clase sobre countries y genericlist

---
+++ Orders.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Orders.Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null!;
+    }
+}
